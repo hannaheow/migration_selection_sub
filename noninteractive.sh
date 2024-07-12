e65754952102ad13bcee2d7ab4fd9a1bc61a8bac
holsonwillia <holsonwillia@ap2002.chtc.wisc.edu>
2024-07-12 09:57:28 -0500

    output for ksim without urb special k

--- noninteractive.sh
+++ noninteractive.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Rscript ksim_scriptforchtc_withreltol.R $@ 
+Rscript ksim_nourb.R $@ 
 echo "listing files in current wd" 
 ls -R 
-echo "looking for bick" 
-find . -name '*bick*' 
+echo "looking for bick_nourb" 
+find . -name '*bick_nourb*' 
 
